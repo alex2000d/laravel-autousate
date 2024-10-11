fdd53b866be8dc1d36b24a7359de48afb475b102
andrearomeo04 <pisco004@gmail.com>
2024-10-11 13:13:56 +0200

    [UPD] created store controller

--- resources/views/admin/autos/create.blade.php
+++ resources/views/admin/autos/create.blade.php
@@ -12,23 +12,27 @@
                 <div class="row">
                     <div class="col-6 mb-3">
                         <label for="" class="control-label">Nome dell'auto</label>
-                        <input type="text" name="title" id="" class="form-control" placeholder="Nome dell'auto">
+                        <input type="text" name="name" id="" class="form-control" placeholder="Nome dell'auto">
                     </div>
                     <div class="col-6 mb-3">
                         <label for="" class="control-label">Tipo di motore</label>
-                        <input type="text" name="title" id="" class="form-control" placeholder="Tipo di motore">
+                        <input type="text" name="engine" id="" class="form-control" placeholder="Tipo di motore">
                     </div>
                     <div class="col-6 mb-3">
                         <label for="" class="control-label">Prezzo</label>
-                        <input type="text" name="title" id="" class="form-control" placeholder="Prezzo">
+                        <input type="text" name="price" id="" class="form-control" placeholder="Prezzo">
                     </div>
                     <div class="col-6 mb-3">
                         <label for="" class="control-label">Quantità</label>
-                        <input type="text" name="title" id="" class="form-control" placeholder="Quantità">
+                        <input type="text" name="quantity" id="" class="form-control" placeholder="Quantità">
+                    </div>
+                    <div class="col-6 mb-3">
+                        <label for="" class="control-label">Optionals</label>
+                        <input type="text" name="optionals" id="" class="form-control" placeholder="Optionals">
                     </div>
                     <div class="col-6 mb-3">
                         <label for="" class="control-label">Status</label>
-                        <input type="text" name="title" id="" class="form-control" placeholder="Status">
+                        <input type="text" name="status" id="" class="form-control" placeholder="Status">
                     </div>
                 </div>
                 <div class="col-6 mb-5">
